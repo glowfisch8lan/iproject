eb1ca4bb9048ebc1d7dfd5283ed3830c583cb4bd
Denis Grigorov <glowfisch8lan@gmail.com>
2021-01-04 12:29:33 +1000

    Написан статический метод сборки Columns

--- modules/system/helpers/GridHelper.php
+++ modules/system/helpers/GridHelper.php
@@ -11,17 +11,17 @@ use yii\helpers\Html;
 class GridHelper extends GridView
 {
 
-    protected function renderFilterCellContent()
-    {
-        return Html::button('Reset', ['class' => 'btn btn-primary']);
-    }
 
     /*
      *  ActionColumnHeader кнопка через определение $data['ActionColumnHeader']
      *  ActionColumnButtons кнопка через определение $data['ActionColumnButtons']
      */
-    public static function initWidget($data = []){
-        $dataProvider = $data['dataProvider'];
+
+    public static $gridData;
+
+    public static function collectColumns(){
+
+        $data = self::$gridData;
 
         $headerCallback = function($url){
             return Html::a('<i class="fa fa-plus" aria-hidden="true"></i></i>', $url,
@@ -31,60 +31,73 @@ class GridHelper extends GridView
 
         $headerActionColumn = empty($data['searchModel']) ? $headerCallback($urlCreate) : null;
         $headerActionColumn = !empty($data['ActionColumnHeader']) ? $data['ActionColumnHeader'] : $headerActionColumn;
-        $ActionColumnButtonsDefault = [
-                'view' => function ($url,$model) {
-                    return Html::a('<i class="fa fa-eye"></i>', $url,
-                        ['class' => 'btn btn-outline-info', 'data-method' => 'post']);
-                },
-                'update' => function ($url,$model) {
-                    return Html::a('<i class="fa fa-pencil" aria-hidden="true"></i>', $url,
-                        ['class' => 'btn btn-outline-info',
-                            'data' => [
-                                'method' => 'post'
-                            ]]);
-                },
 
-                'delete' => function ($url, $model){
-                    return Html::a('<i class="fa fa-trash" aria-hidden="true"></i>', $url,
-                        ['class' => 'btn btn-outline-danger',
-                            'data' => [
-                                'confirm' => 'Вы действительно хотите удалить данную позицию?',
-                                'method' => 'post'
-                            ]]);
-                },
+        $ActionColumnButtonsDefault = [
+            'view' => function ($url,$model) {
+                return Html::a('<i class="fa fa-eye"></i>', $url,
+                    ['class' => 'btn btn-outline-info', 'data-method' => 'post']);
+            },
+            'update' => function ($url,$model) {
+                return Html::a('<i class="fa fa-pencil" aria-hidden="true"></i>', $url,
+                    ['class' => 'btn btn-outline-info',
+                        'data' => [
+                            'method' => 'post'
+                        ]]);
+            },
+
+            'delete' => function ($url, $model){
+                return Html::a('<i class="fa fa-trash" aria-hidden="true"></i>', $url,
+                    ['class' => 'btn btn-outline-danger',
+                        'data' => [
+                            'confirm' => 'Вы действительно хотите удалить данную позицию?',
+                            'method' => 'post'
+                        ]]);
+            },
         ];
         $ActionColumnButtons = !empty($data['ActionColumnButtons']) ? $data['ActionColumnButtons'] : $ActionColumnButtonsDefault;
         $buttonsTemplate = !empty($data['buttonsOptions']['template']) ? $data['buttonsOptions']['template'] : '{update} {delete}';
         $ActionColumnHeaderWidth = !empty($data['buttonsOptions']['headerWidth']) ? $data['buttonsOptions']['headerWidth'] : 150;
 
         $ActionColumnDefault =   [
-           'class' => 'app\modules\system\components\gridviewer\CustomActionColumns',
-           'header' => $headerActionColumn,
-           'template' => $buttonsTemplate,
-           'headerOptions' => [
-               'width' => $ActionColumnHeaderWidth,
-               'style' => 'text-align:center'
-           ],
-           'filterOptions' => ['style' =>'text-align: center;'],
-           'contentOptions'=> ['style' =>'text-align: center;'],
-           'buttons' => $ActionColumnButtons
-       ];
+            'class' => 'app\modules\system\components\gridviewer\CustomActionColumns',
+            'header' => $headerActionColumn,
+            'template' => $buttonsTemplate,
+            'headerOptions' => [
+                'width' => $ActionColumnHeaderWidth,
+                'style' => 'text-align:center'
+            ],
+            'filterOptions' => ['style' =>'text-align: center;'],
+            'contentOptions'=> ['style' =>'text-align: center;'],
+            'buttons' => $ActionColumnButtons
+        ];
 
         $pagerDefault =  [
-           'forcePageParam' => false,
-           'pageSizeParam' => false,
-           'pageSize' => 10
-       ];
+            'forcePageParam' => false,
+            'pageSizeParam' => false,
+            'pageSize' => 10
+        ];
 
-        $searchModel = !empty($data['searchModel']) ? $data['searchModel'] : null;
+
         $ActionColumn = !empty($data['ActionColumn']) ? $data['ActionColumn'] : $ActionColumnDefault;
-        $dataProvider->pagination = !empty($data['pagination']) ? $data['pagination'] : $pagerDefault;
+        $data['dataProvider']->pagination = !empty($data['pagination']) ? $data['pagination'] : $pagerDefault;
 
         $columns = $data['columns'];
         $columns[] = $ActionColumn;
 
+        return $columns;
+    }
 
-       return GridView::widget([
+    public static function initWidget($input = []){
+
+        self::$gridData = $input;
+        $data = self::$gridData;
+
+        $dataProvider = $data['dataProvider'];
+        $searchModel = !empty($data['searchModel']) ? $data['searchModel'] : null;
+
+        $columns = self::collectColumns();
+
+        return GridView::widget([
            'dataProvider' => $dataProvider,
            'filterModel' => $searchModel,
 
