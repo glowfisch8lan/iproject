3f7ee230fc44a1c32d21d2aa411f419c7277afc7
Denis Grigorov <glowfisch8lan@gmail.com>
2021-01-04 12:30:06 +1000

    Разработан интерфейс вывода данных о модуле через GridView

--- modules/system/views/modules/index.php
+++ modules/system/views/modules/index.php
@@ -1,9 +1,58 @@
 <?php
-/* @var $this yii\web\View */
-?>
-<h1>modules/index</h1>
 
-<p>
-    You may change the content of this page by modifying
-    the file <code><?= __FILE__; ?></code>.
-</p>
+use yii\helpers\Html;
+use app\modules\system\helpers\GridHelper;
+
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Модули';
+$this->params['breadcrumbs'][] = ['label' => Yii::$app->controller->module->name, 'url' => '/system/modules'];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="box-body">
+    <div class="col-md-12">
+        <?= GridHelper::initWidget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                ['class' => '\yii\grid\SerialColumn',
+                    'headerOptions' => [
+                        'width' => 50,
+                        'class' => 'text-center'
+                    ],
+                    'contentOptions' => [
+                        'class' => 'text-center'
+                    ],
+                ],
+                [   'attribute' => 'id',
+                    'headerOptions' => [
+                        'width' => 50,
+                        'class' => 'text-center'
+                    ],
+                    'contentOptions' => ['class' => 'text-center'],
+                ],
+
+                [
+                    'attribute' => 'name',
+                ],
+                [
+                    'attribute' => 'status',
+                ],
+            ],
+            'ActionColumnButtons' => [
+                'toggle' => function ($url,$model){
+                    $icons = ($model['status']) ? function(){return ['fa fa-eye', 'btn btn-info','unregister'];} : function(){return ['fa fa-eye-slash', 'btn btn-outline-info','register'];} ;
+
+                    return Html::a('<i class="'.$icons()[0].'"></i>', 'modules/'.$icons()[2].'?id=' . $model['id'],
+                        ['class' => $icons()[1], 'data-method' => 'post']);
+                },
+            ],
+            'ActionColumnHeader' => '&nbsp;',
+            'buttonsOptions' => ['template' => '{toggle}'],
+
+
+        ]);?>
+
+    </div>
+</div>
